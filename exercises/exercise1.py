da6e8cca3d44b3dcb20a529283d00871533abaa7
Nikola Hrabska <83529@student.ue.poznan.pl>
2024-03-13 11:09:42 +0100

    Dodanie trójkąta

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -7,3 +7,9 @@ bok_b = 5
 bok_c = 8
 pole_pr = bok_b * bok_c
 print("Pole prostokąta wynosi: " + str(pole_pr))
+
+#trójkąt
+bok_d = 9
+h = 4
+pole_tr = 1/2 * bok_d * h
+print("Pole trójkąta wynosi: " + str(pole_tr))
