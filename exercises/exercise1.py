97439efd5a74582282888f52145d899dcb0a7c1e
Nikola Hrabska <83529@student.ue.poznan.pl>
2024-03-13 11:06:43 +0100

    Dodanie prostokąta

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,3 +1,9 @@
 bok_a = 7
 pole_kw = bok_a * bok_a
 print("Pole kwadratu wynosi: " + str(pole_kw))
+
+#prostokąt
+bok_b = 5
+bok_c = 8
+pole_pr = bok_b * bok_c
+print("Pole prostokąta wynosi: " + str(pole_pr))
